289f1f936a2ad5fdf986e808f109dd8c21487ba8
mdmarufsarker <mdmarufsarker2021@gmail.com>
2021-11-15 00:45:37 +0600

    Color feature added

---
+++ modules/color.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+const color = {
+  // global colors
+  green: chalk.green.inverse.bold,
+  yellow: chalk.yellow.inverse.bold,
+  cyan: chalk.cyan.inverse.bold,
+  blue: chalk.blue.inverse.bold,
+  magenta: chalk.magenta.inverse.bold,
+  red: chalk.red.inverse.bold,
+  white: chalk.white.inverse.bold,
+  black: chalk.bgBlack.cyan.bold,
+};
+module.exports = color;
